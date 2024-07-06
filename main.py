500c3e5d3873e07e32b866c38a5a6c15edfedc26
YVVV19 <vadimyankovskiy2000@gmail.com>
2024-07-06 10:47:53 +0300

    ...

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ from parsing import Pair, get_symbol_keyboard, get_price
 from data import get_answer
 from keyboard import Answer, answer_keyboard_markup
 
-class Form(StatesGroup):
-    name = State()
-    like_rate = State()
+
+# class Form(StatesGroup):
+#     name = State()
+#     like_rate = State()
 
 
 load_dotenv()
@@ -31,55 +32,51 @@ form_router = Router()
 pair_router = Router()
 
 
+# @form_router.message(CommandStart())
+# async def command_start(message: Message, state: FSMContext) -> None:
+#     await state.clear()
+#     await state.set_state(Form.name)
+#     await message.answer(
+#         "What is your name?",
+#         reply_markup=ReplyKeyboardRemove(),
+#     )
+
+
+# @form_router.message(Command("Cancel"))
+# @form_router.message(F.text.casefold() == "cancel")
+# async def cancel_handler(message: Message, state: FSMContext) -> None:
+#     current_state = await state.get_state()
+#     if current_state is None:
+#         return
+#     logging.info("Cancelling state %r", current_state)
+#     await state.clear()
+#     await message.answer(
+#         "Cancelled.",
+#         reply_markup=ReplyKeyboardRemove(),
+#     )
+
+
 @form_router.message(CommandStart())
-async def command_start(message: Message, state: FSMContext) -> None:
-    await state.clear()
-    await state.set_state(Form.name)
-    await message.answer(
-        "What is your name?",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-
-@form_router.message(Command("Cancel"))
-@form_router.message(F.text.casefold() == "Cancel")
-async def cancel_handler(message: Message, state: FSMContext) -> None:
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-    logging.info("Cancelling state %r", current_state)
-    await state.clear()
-    await message.answer(
-        "Cancelled.",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-
-@form_router.message(Form.name)
 async def process_name(message: Message, state: FSMContext) -> None:
-    await state.update_data(name=message.text)
-    await state.set_state(Form.like_rate)
-    await message.answer(
-        f"Nice to meet you,{html.quote(message.text)}! \nDo you want to see the exchange rates?",
-        )
-    #MAKING KEYBOARD
+    # await state.update_data(name=message.text)
+    # await state.set_state(Form.like_rate)
     data = get_answer()
     markup = answer_keyboard_markup(answer_list=data)
     await message.answer(
-        f"Click yes if you want and no if you don`t",
-        reply_markup=markup,
-    )
+        f"Nice to meet you. \nDo you want to see the exchange rates? \nClick yes if you want and no if you don`t",
+        reply_markup=markup
+        )
    
 
-@form_router.callback_query(Answer.filter(F.name =="no"))
+@form_router.callback_query(Answer.filter(F.name == "no"))
 async def process_dont_want(query: CallbackQuery, callback_data: Answer, state: FSMContext) -> None:
     data = await state.get_data()
     await state.clear()
     await query.message.answer("See you later.", reply_markup=ReplyKeyboardRemove())
-
+    
 
 @form_router.callback_query(Answer.filter())
-async def process_want(query:CallbackQuery, callback_data: Answer) -> None:
+async def process_want(query: CallbackQuery, callback_data: Answer) -> None:
     await query.message.answer(
         "Click on the button of the selected currency",
         reply_markup=get_symbol_keyboard(),
